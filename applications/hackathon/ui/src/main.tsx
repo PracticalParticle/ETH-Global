d8d3cc1fa965ecdca3d2d5af482367d44681cbdd
5ELeeloo <109235352+5ELeeloo@users.noreply.github.com>
2025-11-22 11:20:48 -0300

    tailwind styles

--- applications/hackathon/ui/src/main.tsx
+++ applications/hackathon/ui/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import '@rainbow-me/rainbowkit/styles.css'
+import App from './App.tsx'
 import { CustomWagmiProvider } from './components/WagmiProvider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
